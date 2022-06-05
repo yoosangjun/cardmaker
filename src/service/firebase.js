546ccdb1c3ea32b4b5868a4c23e8762e6aee8b26
yoosangjun <love2ysj@nate.com>
2022-06-06 01:57:41 +0900

    0606

---
+++ src/service/firebase.js
@@ -0,0 +1,11 @@
+import firebase from "firebase";
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_APIKEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+};
+
+// Initialize Firebase
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+export default firebaseApp;
