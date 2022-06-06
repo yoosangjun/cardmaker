3eb33d1c4548fd497f395446a1e87bdc9a8abc53
yoosangjun <love2ysj@nate.com>
2022-06-07 01:10:26 +0900

    0607

--- src/app.jsx
+++ src/app.jsx
@@ -1,10 +1,12 @@
-import "./app.css";
+import React from 'react';
+import styles from './app.module.css';
+import Login from './components/login/login';
 
-function App() {
+function App({ authService }) {
   return (
-    <>
-      <h1>hello</h1>
-    </>
+    <div className={styles.app}>
+      <Login authService={authService} />
+    </div>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,17 +1,13 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./app";
-// import reportWebVitals from './reportWebVitals';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.module.css';
+import App from './app';
+import AuthService from './service/auth_service';
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
+const authService = new AuthService();
+ReactDOM.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <App authService={authService} />
+  </React.StrictMode>,
+  document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// reportWebVitals();
